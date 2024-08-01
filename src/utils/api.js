9938524719836e0b3da8ebfa56b0a4a5fce9ff64
Mani <mani979084@gmail.com>
2024-08-02 03:10:46 +0530

    add live server

--- src/utils/api.js
+++ src/utils/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const getToken = () => localStorage.getItem("token");
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://yelp-camp-server.vercel.app",
   headers: {
     "Content-Type": "application/json",
   },
